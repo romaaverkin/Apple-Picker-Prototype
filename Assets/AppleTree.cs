849d4f685cb08e0c8273bad1a1e7588cd33242dd
Unknown <romaaverkin@gmail.com>
2019-10-07 10:26:11 +0700

    Добавил все файлы

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Шаблон для создания яблок
+    public GameObject applePrefab;
+
+    // Скорость движения яблони
+    public float speed = 1f;
+
+    // Расстояние, на котором должно измениться направление движения яблони
+    public float leftAndRightEdge = 10f;
+
+    // Вероятность случайного изменения направления движения
+    public float chanceToChangeDirections = 0.1f;
+
+    // Частота создания экземпляров яблок
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Сбрасвать яблоки раз в секунду
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Простое перемещение
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Изменение направления
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Начать движение вправо
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Начать движение влево
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Теперь случайная смена направления привязана ко времени,
+        // потому что выполняется в FixedUpdateQ
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // Сменить направление
+        }
+    }
+}
---
+++ Assets/Basket.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Получить ссылку на игровой объект ScoreCounter
+        GameObject scoreGo = GameObject.Find("ScoreCounter");
+        // Получить компонент Text этого игрового объекта
+        scoreGt = scoreGo.GetComponent<Text>();
+        // Установить начальное число очков равным 0
+        scoreGt.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Получить текущие координаты указателя мыши на экране из Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // Координата Z камеры определяет, как далеко в трехмерном пространстве
+        // находится указатель мыши
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Преобразовать точку на двумерной плоскости экрана в трехмерные
+        // координаты игры
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Переместить корзину вдоль оси X в координату X указателя мыши
+        Vector3 pos = transform.position;
+        pos.x = mousePos3D.x;
+        transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Отыскать яблоко, попавшее в эту корзину
+        GameObject collidedWidth = collision.gameObject;
+        if (collidedWidth.tag == "Apple")
+        {
+            Destroy(collidedWidth);
+
+            // Преобразовать текст в scoreGT в целое число
+            int score = int.Parse(scoreGt.text);
+            // Добавить очки за пойманное яблоко
+            score += 100;
+            // Преобразовать число очков обратно в строку и вывести ее на экран
+            scoreGt.text = score.ToString();
+        }
+    }
+}
