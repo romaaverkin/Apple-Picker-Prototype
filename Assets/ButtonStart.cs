541df6eb2d54d8ed3cc34ced8ea54dd9a6acf19b
Unknown <romaaverkin@gmail.com>
2019-10-28 16:41:00 +0700

    Добавил кнопку старт.

---
+++ Assets/ButtonStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonStart : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        //Debug.Log("Start pressed!");
+        SceneManager.LoadScene("_Scene_1");
+    }
+}
